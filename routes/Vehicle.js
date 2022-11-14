8c5c80f9ee97de5752e4fd0fcaef41148758613e
Mamatha-Mallela <111670297+Mamatha-Mallela@users.noreply.github.com>
2022-11-13 22:09:29 -0600

    Page view for creating one instance of my collection

--- routes/Vehicle.js
+++ routes/Vehicle.js
@@ -5,5 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', vehicle_controlers.vehicle_view_all_Page );
 router.get('/detail', vehicle_controlers.vehicle_view_one_Page);
+/* GET create vehicle page */
+router.get('/create', vehicle_controlers.vehicle_create_Page);
 
 module.exports = router;
