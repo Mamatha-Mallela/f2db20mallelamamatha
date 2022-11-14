4925e63595f6b72ce235f3794264f880aed62bd1
Mamatha-Mallela <111670297+Mamatha-Mallela@users.noreply.github.com>
2022-11-13 19:42:37 -0600

    Created page view for displaying one collection

--- controllers/vehicle.js
+++ controllers/vehicle.js
@@ -98,3 +98,17 @@ exports.vehicle_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
+
+// Handle a show one view with id specified by query
+exports.vehicle_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await vehicle.findById( req.query.id)
+    res.render('vehicledetail',
+   { title: 'vehicle Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
--- routes/Vehicle.js
+++ routes/Vehicle.js
@@ -4,5 +4,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', vehicle_controlers.vehicle_view_all_Page );
+router.get('/detail', vehicle_controlers.vehicle_view_one_Page);
 
 module.exports = router;
